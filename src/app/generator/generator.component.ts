643c89d9558a84ba9a5e4d166860c177981c70e1
dianagatcan <gatcan.diana@gmail.com>
2022-01-06 13:26:05 +0200

    create input field

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { max } from 'rxjs';
 
 @Component({
   selector: 'app-generator',
   templateUrl: './generator.component.html',
   styleUrls: ['./generator.component.css']
 })
+
 export class GeneratorComponent implements OnInit {
 dictionary: Array<string> = [];
 letters: string = "";
